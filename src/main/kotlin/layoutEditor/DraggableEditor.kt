61ef52439a544fe028e447d03d264b293765d160
teacons <teacons@fbear.ru>
2022-05-24 18:56:23 +0300

    Исправлено неадекватное трансформирование слоев в DraggableEditor.kt

--- src/main/kotlin/layoutEditor/DraggableEditor.kt
+++ src/main/kotlin/layoutEditor/DraggableEditor.kt
@@ -12,11 +12,14 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clipToBounds
+import androidx.compose.ui.draw.rotate
+import androidx.compose.ui.draw.scale
+import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.input.pointer.consumeAllChanges
 import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.layout.onSizeChanged
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.platform.Typeface
@@ -28,14 +31,27 @@ import androidx.compose.ui.zIndex
 import loadImageBitmap
 import org.jetbrains.skia.FontStyle
 import org.jetbrains.skia.Typeface
+import kotlin.math.PI
+import kotlin.math.cos
 import kotlin.math.roundToInt
+import kotlin.math.sin
+
+fun Offset.rotateBy(angle: Float): Offset {
+    val angleInRadians = angle * PI / 180
+    return Offset(
+        (x * cos(angleInRadians) - y * sin(angleInRadians)).toFloat(),
+        (x * sin(angleInRadians) + y * cos(angleInRadians)).toFloat()
+    )
+}
 
 @Composable
 fun DraggableEditor(layers: List<Layer>, ratio: Float, selectedLayer: Layer?, onSelectedChange: (Layer) -> Unit) {
     Box(
         modifier = Modifier
             .background(Color.Gray)
-            .fillMaxSize(),
+            .fillMaxSize()
+            .onSizeChanged { println(" ${it.height} ${it.width}") }
+        ,
         contentAlignment = Alignment.Center
     ) {
         Box(
@@ -43,26 +59,25 @@ fun DraggableEditor(layers: List<Layer>, ratio: Float, selectedLayer: Layer?, on
                 .padding(4.dp)
                 .clipToBounds()
                 .background(Color.White)
-                .aspectRatio(ratio),
+                .aspectRatio(ratio)
+            ,
             contentAlignment = Alignment.Center
         ) {
             layers.forEachIndexed { index, layer ->
                 val editingModifier =
                     Modifier
                         .offset { IntOffset(layer.offset.value.x.roundToInt(), layer.offset.value.y.roundToInt()) }
+                        .scale(layer.scale.value)
+                        .rotate(layer.rotation.value)
                         .pointerInput(layer) {
                             detectDragGestures(
                                 onDragStart = { onSelectedChange(layer) },
                             ) { change, dragAmount ->
                                 change.consumeAllChanges()
-                                layer.offset.value += dragAmount
+                                layer.offset.value =
+                                    ((layer.offset.value / layer.scale.value) + dragAmount.rotateBy(layer.rotation.value)) * layer.scale.value
                             }
                         }
-                        .graphicsLayer(
-                            scaleX = layer.scale.value,
-                            scaleY = layer.scale.value,
-                            rotationZ = layer.rotation.value,
-                        )
                         .then(if (selectedLayer == layer) Modifier.border(2.dp, Color.Black) else Modifier)
                         .clickable { onSelectedChange(layer) }
                         .zIndex(index.toFloat())
